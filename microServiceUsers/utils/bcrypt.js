d979dc8e44f748ca0fb0a5e56c1e86e081c459a7
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-08 17:59:56 +0200

    intialized response file and finished bcrypt

---
+++ microServiceUsers/utils/bcrypt.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const saltRounds = parseInt(process.env.SALT_ROUNDS) ;
+
+const hashPassword = async (passwordRecieved) => {
+    try {
+      const hashedPassword = await bcrypt.hash(passwordRecieved, saltRounds);
+      return hashedPassword;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+   
+  };
+
+  const verifyPassword = async (passwordRecieved, hashedPassword) => {
+    try {
+      const passwordsMatch = await bcrypt.compare(passwordRecieved, hashedPassword);
+      return passwordsMatch;
+    } catch (err) {
+      throw new Error("Error comparing passwords: " + err.message);
+    }
+  };
+
+
+
+module.exports = { verifyPassword, hashPassword };
