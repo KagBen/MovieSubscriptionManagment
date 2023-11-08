1e95f18df4adbc1361cfecda13998783c1ab3b1e
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-08 16:21:15 +0200

    add msUsers with almost all server done

---
+++ microServiceUsers/config/mongoDb.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoDbPath = process.env.MONGODB_PATH || "mongodb://localhost:27017/cinemaManagment";
+
+const connectDb = () => {
+  mongoose
+    .connect(mongoDbPath)
+    .then(console.log("Connected to MongoDB server at " + mongoDbPath))
+    .catch((err) => {
+      console.log("Failed to connect to MongoDB server dut to : " + err);
+    });
+};
+
+module.exports = connectDb;
