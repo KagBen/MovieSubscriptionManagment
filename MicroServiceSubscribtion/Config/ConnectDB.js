ec14f74b50b3248897be639267d7388135423695
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-08 18:23:03 +0200

    change names

--- MicroServiceSubscribtion/Config/ConnectDB.js
+++ MicroServiceSubscribtion/Config/ConnectDB.js
@@ -3,11 +3,11 @@ require("dotenv").config()
 
 const mongoosePath = process.env.MONGODB_PATH  || 'mongodb://localhost:27017/cinemaManagmentSubscription'
 
-const ConectDB = () => {
+const ConnectDB = () => {
     mongoose.connect(mongoosePath).then(() => {
         console.log("Connect to Cinema Managment data base.")
     }).catch(err => console.log(err))
 
 }
 
-module.exports = ConectDB
+module.exports = ConnectDB
--- MicroServiceSubscribtion/index.js
+++ MicroServiceSubscribtion/index.js
@@ -1,12 +1,12 @@
 const express = require("express")
 require("dotenv").config()
 const cors = require('cors')
-const ConectDB = require("./Config/ConectDB.js")
+const ConnectDB = require("./Config/ConectDB.js")
 const port = process.env.PORT || 3002
 
 const app = express()
 
-ConectDB()
+ConnectDB()
 
 app.use(cors())
 app.use(express.json())
