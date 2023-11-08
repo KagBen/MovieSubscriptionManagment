ba17b2a0c4ff6a5d29c37217657efc39e4992849
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-08 18:21:16 +0200

    prettier

--- MicroServiceSubscribtion/Dal/MoviesDal.js
+++ MicroServiceSubscribtion/Dal/MoviesDal.js
@@ -1,37 +1,32 @@
-const axios = require("axios")
-const Movie = require("../Models/Movies")
-const MoviesUrl = `https://api.tvmaze.com/shows`
-
+const axios = require("axios");
+const Movie = require("../Models/Movies");
+const MoviesUrl = `https://api.tvmaze.com/shows`;
 
 const GetAllMovies = async () => {
-    try {
-        const resp = await axios.get(MoviesUrl)
-        const data = await resp.json
+  try {
+    const resp = await axios.get(MoviesUrl);
+    const data = await resp.json;
 
-        if (resp.status === 200) {
+    if (resp.status === 200) {
+      for (const movie of data) {
+        const { name, genres, image, premiered } = movie;
 
-            for (const movie of data) {
-                const { name, genres, image, premiered } = movie;
+        const newMovie = new Movie({
+          Name: name,
+          Genres: genres,
+          Image: image ? image.medium : "",
+          Premiered: premiered,
+        });
 
-                const newMovie = new Movie({
-                    Name: name,
-                    Genres: genres,
-                    Image: image ? image.medium : '',
-                    Premiered: premiered
-                });
-
-                await newMovie.save();
-            }
-            console.log('All movies have been saved to the database.');
-        }
-        else {
-            throw new Error({ message: "Cannot get movies from url - " + MoviesUrl })
-        }
-
-    } catch (error) {
-        throw new Error({ message: error.message });
+        await newMovie.save();
+      }
+      console.log("All movies have been saved to the database.");
+    } else {
+      throw new Error({ message: "Cannot get movies from url - " + MoviesUrl });
     }
+  } catch (error) {
+    throw new Error({ message: error.message });
+  }
 };
 
-
-module.exports = GetAllMovies
+module.exports = GetAllMovies;
