ce60caf75db5266fb695cd1972735d34e525950f
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-09 16:33:59 +0200

    add logic subscriptions

---
+++ MicroServiceSubscribtion/Bll/SubscriptionsBll.js
@@ -0,0 +1,19 @@
+const Subscription = require("../Models/Subscriptions");
+
+const addSubcription = async () => {};
+
+const updateSubscription = async (subscriptionId, updateFields) => {};
+
+const getAllSubscriptions = () => {};
+
+const getSubscriptionByMemberId = async (memberId) => {};
+
+const getAllMoviesSubscribersByMovieId = (movieId) => {};
+
+module.exports = {
+  addSubcription,
+  getAllMoviesSubscribersByMovieId,
+  getAllSubscriptions,
+  updateSubscription,
+  getSubscriptionByMemberId,
+};
