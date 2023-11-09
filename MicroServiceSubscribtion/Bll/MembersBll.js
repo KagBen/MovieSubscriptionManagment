fb8d4cee2bc2947fed0ac819a395386b43f39bc4
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-09 16:00:50 +0200

    done memebersBll

--- MicroServiceSubscribtion/Bll/MembersBll.js
+++ MicroServiceSubscribtion/Bll/MembersBll.js
@@ -1,1 +1,43 @@
+const Member = require("../Models/MemberModel");
 
+const getMembers = async () => {
+  return Member.find();
+};
+
+const getMemberById = async (memberId) => {
+  return Member.findById(memberId);
+};
+
+const deleteMember = async (memberId) => {
+  Member.findByIdAndDelete(memberId);
+  return "deleted";
+};
+
+const updateMember = async (memberId, updateFields) => {
+  try {
+    if(updateFields.hasOwnProperty("Name") && updateFields.Name.length === 0 )
+    {
+        throw new Error("Error : Trying to update member with empty name");
+    }
+    const updateMember = Member.findOneAndUpdate(
+      { _id: userId }, // Filter: Update the user with the specified userId
+      { $set: updateFields }, // Update: Fields to be updated
+      { new: true }
+    );
+    if (!updateMember) {
+      // Handle the case when the user with the given userId is not found
+      throw new Error("Member not found");
+    }
+    return updateMember;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const addMember = async(memberObj) => { 
+   const newMember = new Member(memberObj);
+   await newMember.save();
+   return newMember;
+}
+
+module.exports = {getMembers,getMemberById,deleteMember,updateMember ,addMember}
