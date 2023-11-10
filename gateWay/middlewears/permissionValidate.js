dd4470e3268619ae75da8b37c728664124ce614e
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-10 12:33:19 +0200

    f

--- gateWay/middlewears/permissionValidate.js
+++ gateWay/middlewears/permissionValidate.js
@@ -1,15 +1,16 @@
 const permissionValidate = (param) => {
-  return (req, res, next) => {
-    const permission = req.session.permission;
-    const found = permission.find((p) => p === param);
-    if (!found) {
-      return res.status(401).json({
-        message: `User Not Have Permission to this action : ${param}`,
-      });
-    }
-    // Call next() to pass control to the next middleware or route handler
-    next();
+    return (req, res, next) => {
+      const permissions = req.session.permissions || []; // Ensure permissions is an array
+      const found = permissions.some((p) => p === param);
+      if (!found) {
+        return res.status(401).json({
+          message: `User does not have permission for this action: ${param}`,
+        });
+      }
+      // Call next() to pass control to the next middleware or route handler
+      next();
+    };
   };
-};
-
-module.exports = permissionValidate;
+  
+  module.exports = permissionValidate;
+  
