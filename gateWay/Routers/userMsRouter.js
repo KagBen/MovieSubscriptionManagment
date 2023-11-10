bd7346549492837f0b9cde6f0bb12af3e15ac6de
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-10 12:10:14 +0200

    f

--- gateWay/Routers/userMsRouter.js
+++ gateWay/Routers/userMsRouter.js
@@ -5,19 +5,53 @@ const jwt = require("jsonwebtoken");
 const msUsersRouter = express.Router();
 const userMs = "http://localhost:3001/users";
 
-
 msUsersRouter.post("/login", async (req, res) => {
-    try {
-        console.log({userLoginInfo:req.body.userLoginInfo})
-      const userResponse = await axios.post(
-        `${userMs}/login`,
-        {userLoginInfo:req.body.userLoginInfo}
-      );
-      console.log(userResponse.data); 
-      res.send(userResponse.data);
-    } catch (err) {
-    res.status(401).send(err.response.data);
+  try {
+    const userResponse = await axios.post(`${userMs}/login`, {
+      userLoginInfo: req.body.userLoginInfo,
+    });
+    // Check if userResponse.data.user is defined
+    if (userResponse.data.user) {
+      let token;
+      if (userResponse.data.user.sessionTimeOut === null) {
+        token = jwt.sign(
+          { user: userResponse.data.user },
+          process.env.JWT_ACCESS_SECRET_TOKEN
+        );
+
+        req.session.cookie.maxAge = null;
+      } else {
+        // If sessionTimeOut is a valid value, set expiration to that value in minutes
+        const expiresIn = `${userResponse.data.user.sessionTimeOut}m`;
+        token = jwt.sign(
+          { user: userResponse.data.user },
+          process.env.JWT_ACCESS_SECRET_TOKEN,
+          { expiresIn }
+        );
+        // Set the maxAge dynamically in the session cookie
+        req.session.cookie.maxAge =
+          userResponse.data.user.sessionTimeOut * 60 * 1000;
+      }
+      req.session.role = userResponse.data.user.role;
+      req.session.user.permissions = userResponse.data.user.permissions;
+      res.status(200).send({ user: userResponse.data.user, token });
+    } else {
+      // Handle the case where userResponse.data.user is not defined
+      res.status(401).send({ message: "User not found", detailedError: {} });
     }
-  });
-  
+  } catch (err) {
+    if (err.response && err.response.data) {
+      // Send the error response if it exists
+      res.status(401).send(err.response.data);
+    } else {
+      // Handle the case where err.response or err.response.data is undefined
+      res.status(401).send({ message: "Unauthorized", detailedError: {} });
+    }
+  }
+});
+
+msUsersRouter.get("/logout", async (req, res) => {
+  req.session.destroy();
+});
+
 module.exports = msUsersRouter;
