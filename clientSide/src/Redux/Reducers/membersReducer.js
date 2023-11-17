9cc6c03286462799a6ce299aacf2da33fbfca2a3
Ben Kagarlizky <kagarlizky.ab@gmail.com>
2023-11-17 17:11:35 +0200

    redux

---
+++ clientSide/src/Redux/Reducers/membersReducer.js
@@ -0,0 +1,50 @@
+const initialState = {
+  members: [],
+  membersLoading: false,
+};
+
+const membersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_MEMBERS": {
+      return { ...state, members: action.payload };
+    }
+
+    case "ADD_MEMBER": {
+      const addedMembersToOthers = [...state.members, action.payload];
+      return { ...state, members: addedMembersToOthers };
+    }
+
+    //in update we find the member delete him and add again .
+    case "UPDATE_MEMBER": {
+      const memberIndex = state.members.findIndex(
+        (member) => member._id === action.payload._id
+      );
+      // Create a new array with the updated movie
+      const updatedMembers = [
+        ...state.members.slice(0, memberIndex),
+        action.payload, // Updated movie
+        ...state.members.slice(memberIndex + 1),
+      ];
+      return {
+        ...state,
+        members: updatedMembers,
+      };
+    }
+
+    case "DELETE_MEMBER": {
+      return {
+        ...state,
+        members: state.members.filter(
+          (member) => member._id !== action.payload
+        ),
+      };
+    }
+
+    case "SET_MEMBER_LOADING": {
+      return { ...state, membersLoading: action.payload };
+    }
+
+    default:
+      return state;
+  }
+};
---
+++ clientSide/src/Redux/Reducers/moviesReducer.js
@@ -0,0 +1,47 @@
+const initialState = {
+  movies: [],
+  moviesLoading: false,
+};
+
+const moviesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_MOVIES": {
+      return { ...state, movies: action.payload };
+    }
+    case "ADD_MOVIE": {
+      const addedMoviesToOthers = [...state.movies, action.payload];
+      return { ...state, movies: addedMoviesToOthers };
+    }
+    
+    //in update we find the movie delete him and add again .
+    case "UPDATE_MOVIE": {
+      const movieIndex = state.movies.findIndex(
+        (movie) => movie._id === action.payload._id
+      );
+      // Create a new array with the updated movie
+      const updatedMovies = [
+        ...state.movies.slice(0, movieIndex),
+        action.payload, // Updated movie
+        ...state.movies.slice(movieIndex + 1),
+      ];
+      return {
+        ...state,
+        movies: updatedMovies
+      };
+    }
+
+    case "DELETE_MOVIE": {
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie._id !== action.payload),
+      };
+    }
+
+    case "SET_MOVIES_LOADING": {
+      return { ...state, moviesLoading: action.payload };
+    }
+
+    default:
+      return state;
+  }
+};
