3e7500e4613f1f00a506488508d7ecd46439e770
Anna Kagarlizky <ba.kagarlizky@gmail.com>
2023-12-15 11:57:47 +0200

    1

--- clientSide/src/Pages/MoviesPage/MoviePage.jsx
+++ clientSide/src/Pages/MoviesPage/MoviePage.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from "react-redux";
+
 
 const MoviePage = () => {
+
+  const AllMovies = useSelector(state => state.movies.movies)
+  console.log(AllMovies)
+//const movie = AllMovies.find(movie => movie._id ===  )
+
   return (
     <div>MoviePage</div>
   )
